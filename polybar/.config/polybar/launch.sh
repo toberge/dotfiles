d2ec4221d0ac770987caec92f710a0159e9561b2
Tore Bergebakken <toberge@stud.ntnu.no>
2020-06-08 21:31:03 +0200

    Calendar thing in polybar + bar split

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -14,15 +14,22 @@ done
 
 # Start bar on all monitors
 # from https://github.com/gugahoi/dotfiles-linux/blob/master/config/polybar/scripts/launch.sh
-name=main
+primary=$(xrandr --query | grep " primary" | cut -d' ' -f1)
+mainbar=main
+lamebar=secondary
 if type "xrandr"
 then
-  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1)
-  do
-    MONITOR=${monitor} polybar --reload ${name}&
-  done
+    for monitor in $(xrandr --query | grep " connected" | cut -d' ' -f1)
+    do
+        if [[ "${monitor}" = "${primary}" ]]
+        then
+            MONITOR=${monitor} polybar --reload ${mainbar}&
+        else
+            MONITOR=${monitor} polybar --reload ${lamebar}&
+        fi
+    done
 else
-  polybar --reload ${name} &
+  polybar --reload ${mainbar} &
 fi
 
-echo "Launched your bar(s), ${name}"
+echo "Launched your bar(s), ${mainbar} and ${lamebar}"
