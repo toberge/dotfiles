af9f47930a68eb200c7641edf7c32a89b3aba813
Tore Bergebakken <toberge@stud.ntnu.no>
2020-06-08 21:28:46 +0200

    monitor stuff, xmodmap rename

--- common/.local/bin/autostart.sh
+++ common/.local/bin/autostart.sh
@@ -6,8 +6,9 @@ sfx startup &
 # monitor layout and device specifics
 case "$HOSTNAME" in
     fuglekassa) # desktop
-        ~/.local/bin/toggletearing.sh
-        ~/.screenlayout/default.sh
+        # ~/.local/bin/toggletearing.sh
+        [[ "$(xrandr --query | grep -c " connected")" -eq 2 ]] \
+            && xrandr --output HDMI-0 --auto --right-of DVI-D-0
         ;;
     thinkpad)
         [[ "$(xrandr --query | grep -c " connected")" -eq 2 ]] \
@@ -28,12 +29,12 @@ picom -b --dbus --config ~/.config/picom/picom.conf
 # numlock, I need you!
 numlockx on
 # Caps Lock go bye bye
-xmodmap ~/.xmodmap
+xmodmap ~/.Xmodmap
 # and become Escape
 xcape
 
 # increase speed of spamming
-xset r rate 300 45
+xset r rate 360 42
 
 # just in case wm/de does not set cursor correctly
 xsetroot -cursor_name left_ptr
@@ -63,4 +64,3 @@ do
         nohup $app &
     fi
 done
-
