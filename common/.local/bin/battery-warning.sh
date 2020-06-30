6e4a7e3cfff96d9b201b53eaf4b5ec3032040ed6
Tore Bergebakken <toberge@stud.ntnu.no>
2020-06-30 19:56:43 +0200

    Make battery script use dunstify

--- common/.local/bin/battery-warning.sh
+++ common/.local/bin/battery-warning.sh
@@ -17,8 +17,9 @@ do # Check battery level every X seconds
     read -r lvl < /sys/class/power_supply/BAT0/capacity
     read -r stat < /sys/class/power_supply/BAT0/status
     [[ $lvl -le $critical_level ]] && [[ "$stat" == "Discharging" ]] && {
-        notify-send --urgency=critical \
+        dunstify --urgency=critical \
             --icon=battery \
+            --replace=9999 \
             "BATTERY LOW" \
             "[${lvl}%] Plug in your charger"
         sfx alert
